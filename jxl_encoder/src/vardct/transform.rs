10e796f207fd35ae1b6f57fc6d17f251c112bdf9
Lilith River <lilith@imazen.io>
2026-02-23 17:37:35 -0700

    refactor: replace try_into().expect() with as_array_ref in transform.rs
    Replace 26 slice-to-array conversions that copied data to the stack with
    direct references via as_array_ref/as_array_mut helpers. For the 64x-family
    dc_from functions (which take &[f32] slices), pass the Vec directly.
    
    Eliminates unnecessary stack copies of up to 16KB (DCT64x64) per call and
    removes expect() panic strings from the binary. No instruction count change
    in release (LLVM already optimized most copies away), but cleaner code.

--- jxl_encoder/src/vardct/transform.rs
+++ jxl_encoder/src/vardct/transform.rs
@@ -428,30 +428,21 @@ impl VarDctEncoder {
                             quant_dc[1][by][bx] = (dct_coeffs[1][0] * inv_factor).round() as i16;
                         }
                         RAW_STRATEGY_DCT16X8 => {
-                            let coeffs_arr: [f32; 128] = dct_coeffs[1][..128]
-                                .try_into()
-                                .expect("128 coefficients for DCT16x8");
-                            let dcs = dc_from_dct_16x8(&coeffs_arr);
+                            let dcs = dc_from_dct_16x8(as_array_ref::<128>(&dct_coeffs[1], 0));
                             for iy in 0..2 {
                                 float_dc[1][(by + iy) * xsize_blocks + bx] = dcs[iy];
                                 quant_dc[1][by + iy][bx] = (dcs[iy] * inv_factor).round() as i16;
                             }
                         }
                         RAW_STRATEGY_DCT8X16 => {
-                            let coeffs_arr: [f32; 128] = dct_coeffs[1][..128]
-                                .try_into()
-                                .expect("128 coefficients for DCT8x16");
-                            let dcs = dc_from_dct_8x16(&coeffs_arr);
+                            let dcs = dc_from_dct_8x16(as_array_ref::<128>(&dct_coeffs[1], 0));
                             for ix in 0..2 {
                                 float_dc[1][by * xsize_blocks + bx + ix] = dcs[ix];
                                 quant_dc[1][by][bx + ix] = (dcs[ix] * inv_factor).round() as i16;
                             }
                         }
                         RAW_STRATEGY_DCT16X16 => {
-                            let coeffs_arr: [f32; 256] = dct_coeffs[1][..256]
-                                .try_into()
-                                .expect("256 coefficients for DCT16x16");
-                            let dcs = dc_from_dct_16x16(&coeffs_arr);
+                            let dcs = dc_from_dct_16x16(as_array_ref::<256>(&dct_coeffs[1], 0));
                             // dcs = [dc00, dc01, dc10, dc11] in row-major 2x2
                             #[cfg(feature = "debug-dc")]
                             eprintln!(
@@ -486,10 +477,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT32X32 => {
-                            let coeffs_arr: [f32; 1024] = dct_coeffs[1][..1024]
-                                .try_into()
-                                .expect("1024 coefficients for DCT32x32");
-                            let dcs = dc_from_dct_32x32(&coeffs_arr);
+                            let dcs = dc_from_dct_32x32(as_array_ref::<1024>(&dct_coeffs[1], 0));
                             #[cfg(feature = "debug-dc")]
                             eprintln!(
                                 "DCT32x32 block (by={}, bx={}): dcs[0..4]=[{:.4}, {:.4}, {:.4}, {:.4}], LLF=[{:.6}, {:.6}, {:.6}, {:.6}]",
@@ -499,10 +487,10 @@ impl VarDctEncoder {
                                 dcs[1],
                                 dcs[2],
                                 dcs[3],
-                                coeffs_arr[0],
-                                coeffs_arr[1],
-                                coeffs_arr[32],
-                                coeffs_arr[33]
+                                dct_coeffs[1][0],
+                                dct_coeffs[1][1],
+                                dct_coeffs[1][32],
+                                dct_coeffs[1][33]
                             );
                             // dcs = 16 DC values in row-major 4x4
                             for iy in 0..4 {
@@ -524,10 +512,7 @@ impl VarDctEncoder {
                         }
                         RAW_STRATEGY_DCT32X16 => {
                             // DCT32X16: 4 block rows × 2 block cols, returns 8 DC values in 4×2 order
-                            let coeffs_arr: [f32; 512] = dct_coeffs[1][..512]
-                                .try_into()
-                                .expect("512 coefficients for DCT32x16");
-                            let dcs = dc_from_dct_32x16(&coeffs_arr);
+                            let dcs = dc_from_dct_32x16(as_array_ref::<512>(&dct_coeffs[1], 0));
                             for iy in 0..4 {
                                 for ix in 0..2 {
                                     float_dc[1][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -539,10 +524,7 @@ impl VarDctEncoder {
                         }
                         RAW_STRATEGY_DCT16X32 => {
                             // DCT16X32: 2×4 blocks, returns 8 DC values in row-major 2x4
-                            let coeffs_arr: [f32; 512] = dct_coeffs[1][..512]
-                                .try_into()
-                                .expect("512 coefficients for DCT16x32");
-                            let dcs = dc_from_dct_16x32(&coeffs_arr);
+                            let dcs = dc_from_dct_16x32(as_array_ref::<512>(&dct_coeffs[1], 0));
                             for iy in 0..2 {
                                 for ix in 0..4 {
                                     float_dc[1][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -554,10 +536,7 @@ impl VarDctEncoder {
                         }
                         RAW_STRATEGY_DCT64X64 => {
                             // DCT64X64: 8×8 blocks, returns 64 DC values in row-major 8x8
-                            let coeffs_arr: [f32; 4096] = dct_coeffs[1][..4096]
-                                .try_into()
-                                .expect("4096 coefficients for DCT64x64");
-                            let dcs = dc_from_dct_64x64(&coeffs_arr);
+                            let dcs = dc_from_dct_64x64(&dct_coeffs[1]);
                             for iy in 0..8 {
                                 for ix in 0..8 {
                                     float_dc[1][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -569,10 +548,7 @@ impl VarDctEncoder {
                         }
                         RAW_STRATEGY_DCT64X32 => {
                             // DCT64X32: 8 block rows × 4 block cols, returns 32 DC values in 8×4 order
-                            let coeffs_arr: [f32; 2048] = dct_coeffs[1][..2048]
-                                .try_into()
-                                .expect("2048 coefficients for DCT64x32");
-                            let dcs = dc_from_dct_64x32(&coeffs_arr);
+                            let dcs = dc_from_dct_64x32(&dct_coeffs[1]);
                             for iy in 0..8 {
                                 for ix in 0..4 {
                                     float_dc[1][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -584,10 +560,7 @@ impl VarDctEncoder {
                         }
                         RAW_STRATEGY_DCT32X64 => {
                             // DCT32X64: 4×8 blocks, returns 32 DC values in row-major 4x8
-                            let coeffs_arr: [f32; 2048] = dct_coeffs[1][..2048]
-                                .try_into()
-                                .expect("2048 coefficients for DCT32x64");
-                            let dcs = dc_from_dct_32x64(&coeffs_arr);
+                            let dcs = dc_from_dct_32x64(&dct_coeffs[1]);
                             for iy in 0..4 {
                                 for ix in 0..8 {
                                     float_dc[1][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -598,39 +571,23 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT4X8 => {
-                            // DCT4X8 full covers 1×1 blocks, returns single DC
-                            let coeffs_arr: [f32; 64] = dct_coeffs[1][..64]
-                                .try_into()
-                                .expect("64 coefficients for DCT4X8");
-                            let dc = dc_from_dct_4x8_full(&coeffs_arr);
+                            let dc = dc_from_dct_4x8_full(as_array_ref::<64>(&dct_coeffs[1], 0));
                             float_dc[1][by * xsize_blocks + bx] = dc;
                             quant_dc[1][by][bx] = (dc * inv_factor).round() as i16;
                         }
                         RAW_STRATEGY_DCT8X4 => {
-                            // DCT8X4 full covers 1×1 blocks, returns single DC
-                            let coeffs_arr: [f32; 64] = dct_coeffs[1][..64]
-                                .try_into()
-                                .expect("64 coefficients for DCT8X4");
-                            let dc = dc_from_dct_8x4_full(&coeffs_arr);
+                            let dc = dc_from_dct_8x4_full(as_array_ref::<64>(&dct_coeffs[1], 0));
                             float_dc[1][by * xsize_blocks + bx] = dc;
                             quant_dc[1][by][bx] = (dc * inv_factor).round() as i16;
                         }
                         RAW_STRATEGY_DCT4X4 => {
-                            // DCT4X4 full covers 1×1 blocks, returns single DC
-                            let coeffs_arr: [f32; 64] = dct_coeffs[1][..64]
-                                .try_into()
-                                .expect("64 coefficients for DCT4X4");
-                            let dc = dc_from_dct_4x4_full(&coeffs_arr);
+                            let dc = dc_from_dct_4x4_full(as_array_ref::<64>(&dct_coeffs[1], 0));
                             float_dc[1][by * xsize_blocks + bx] = dc;
                             quant_dc[1][by][bx] = (dc * inv_factor).round() as i16;
                         }
                         RAW_STRATEGY_AFV0 | RAW_STRATEGY_AFV1 | RAW_STRATEGY_AFV2
                         | RAW_STRATEGY_AFV3 => {
-                            // AFV covers 1×1 blocks, returns single DC
-                            let coeffs_arr: [f32; 64] = dct_coeffs[1][..64]
-                                .try_into()
-                                .expect("64 coefficients for AFV");
-                            let dc = dc_from_afv(&coeffs_arr);
+                            let dc = dc_from_afv(as_array_ref::<64>(&dct_coeffs[1], 0));
                             float_dc[1][by * xsize_blocks + bx] = dc;
                             quant_dc[1][by][bx] = (dc * inv_factor).round() as i16;
                         }
@@ -869,10 +826,7 @@ impl VarDctEncoder {
                                 (dc * inv_factor - y_dc * dc_cfl_factor).round() as i16;
                         }
                         RAW_STRATEGY_DCT16X8 => {
-                            let coeffs_arr: [f32; 128] = dct_coeffs[c][..128]
-                                .try_into()
-                                .expect("128 coefficients for DCT16x8");
-                            let dcs = dc_from_dct_16x8(&coeffs_arr);
+                            let dcs = dc_from_dct_16x8(as_array_ref::<128>(&dct_coeffs[c], 0));
                             for iy in 0..2 {
                                 float_dc[c][(by + iy) * xsize_blocks + bx] = dcs[iy];
                                 let y_dc = quant_dc[1][by + iy][bx] as f32;
@@ -881,10 +835,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT8X16 => {
-                            let coeffs_arr: [f32; 128] = dct_coeffs[c][..128]
-                                .try_into()
-                                .expect("128 coefficients for DCT8x16");
-                            let dcs = dc_from_dct_8x16(&coeffs_arr);
+                            let dcs = dc_from_dct_8x16(as_array_ref::<128>(&dct_coeffs[c], 0));
                             for ix in 0..2 {
                                 float_dc[c][by * xsize_blocks + bx + ix] = dcs[ix];
                                 let y_dc = quant_dc[1][by][bx + ix] as f32;
@@ -893,10 +844,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT16X16 => {
-                            let coeffs_arr: [f32; 256] = dct_coeffs[c][..256]
-                                .try_into()
-                                .expect("256 coefficients for DCT16x16");
-                            let dcs = dc_from_dct_16x16(&coeffs_arr);
+                            let dcs = dc_from_dct_16x16(as_array_ref::<256>(&dct_coeffs[c], 0));
                             for iy in 0..2 {
                                 for ix in 0..2 {
                                     float_dc[c][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -909,10 +857,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT32X32 => {
-                            let coeffs_arr: [f32; 1024] = dct_coeffs[c][..1024]
-                                .try_into()
-                                .expect("1024 coefficients for DCT32x32");
-                            let dcs = dc_from_dct_32x32(&coeffs_arr);
+                            let dcs = dc_from_dct_32x32(as_array_ref::<1024>(&dct_coeffs[c], 0));
                             for iy in 0..4 {
                                 for ix in 0..4 {
                                     float_dc[c][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -925,11 +870,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT32X16 => {
-                            // DCT32X16: 2 cols × 4 rows coverage
-                            let coeffs_arr: [f32; 512] = dct_coeffs[c][..512]
-                                .try_into()
-                                .expect("512 coefficients for DCT32x16");
-                            let dcs = dc_from_dct_32x16(&coeffs_arr);
+                            let dcs = dc_from_dct_32x16(as_array_ref::<512>(&dct_coeffs[c], 0));
                             for iy in 0..4 {
                                 for ix in 0..2 {
                                     float_dc[c][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -942,11 +883,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT16X32 => {
-                            // DCT16X32: 4 cols × 2 rows coverage
-                            let coeffs_arr: [f32; 512] = dct_coeffs[c][..512]
-                                .try_into()
-                                .expect("512 coefficients for DCT16x32");
-                            let dcs = dc_from_dct_16x32(&coeffs_arr);
+                            let dcs = dc_from_dct_16x32(as_array_ref::<512>(&dct_coeffs[c], 0));
                             for iy in 0..2 {
                                 for ix in 0..4 {
                                     float_dc[c][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -959,10 +896,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT64X64 => {
-                            let coeffs_arr: [f32; 4096] = dct_coeffs[c][..4096]
-                                .try_into()
-                                .expect("4096 coefficients for DCT64x64");
-                            let dcs = dc_from_dct_64x64(&coeffs_arr);
+                            let dcs = dc_from_dct_64x64(&dct_coeffs[c]);
                             for iy in 0..8 {
                                 for ix in 0..8 {
                                     float_dc[c][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -975,10 +909,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT64X32 => {
-                            let coeffs_arr: [f32; 2048] = dct_coeffs[c][..2048]
-                                .try_into()
-                                .expect("2048 coefficients for DCT64x32");
-                            let dcs = dc_from_dct_64x32(&coeffs_arr);
+                            let dcs = dc_from_dct_64x32(&dct_coeffs[c]);
                             for iy in 0..8 {
                                 for ix in 0..4 {
                                     float_dc[c][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -991,10 +922,7 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT32X64 => {
-                            let coeffs_arr: [f32; 2048] = dct_coeffs[c][..2048]
-                                .try_into()
-                                .expect("2048 coefficients for DCT32x64");
-                            let dcs = dc_from_dct_32x64(&coeffs_arr);
+                            let dcs = dc_from_dct_32x64(&dct_coeffs[c]);
                             for iy in 0..4 {
                                 for ix in 0..8 {
                                     float_dc[c][(by + iy) * xsize_blocks + (bx + ix)] =
@@ -1007,30 +935,21 @@ impl VarDctEncoder {
                             }
                         }
                         RAW_STRATEGY_DCT4X8 => {
-                            let coeffs_arr: [f32; 64] = dct_coeffs[c][..64]
-                                .try_into()
-                                .expect("64 coefficients for DCT4X8");
-                            let dc = dc_from_dct_4x8_full(&coeffs_arr);
+                            let dc = dc_from_dct_4x8_full(as_array_ref::<64>(&dct_coeffs[c], 0));
                             float_dc[c][by * xsize_blocks + bx] = dc;
                             let y_dc = quant_dc[1][by][bx] as f32;
                             quant_dc[c][by][bx] =
                                 (dc * inv_factor - y_dc * dc_cfl_factor).round() as i16;
                         }
                         RAW_STRATEGY_DCT8X4 => {
-                            let coeffs_arr: [f32; 64] = dct_coeffs[c][..64]
-                                .try_into()
-                                .expect("64 coefficients for DCT8X4");
-                            let dc = dc_from_dct_8x4_full(&coeffs_arr);
+                            let dc = dc_from_dct_8x4_full(as_array_ref::<64>(&dct_coeffs[c], 0));
                             float_dc[c][by * xsize_blocks + bx] = dc;
                             let y_dc = quant_dc[1][by][bx] as f32;
                             quant_dc[c][by][bx] =
                                 (dc * inv_factor - y_dc * dc_cfl_factor).round() as i16;
                         }
                         RAW_STRATEGY_DCT4X4 => {
-                            let coeffs_arr: [f32; 64] = dct_coeffs[c][..64]
-                                .try_into()
-                                .expect("64 coefficients for DCT4X4");
-                            let dc = dc_from_dct_4x4_full(&coeffs_arr);
+                            let dc = dc_from_dct_4x4_full(as_array_ref::<64>(&dct_coeffs[c], 0));
                             float_dc[c][by * xsize_blocks + bx] = dc;
                             let y_dc = quant_dc[1][by][bx] as f32;
                             quant_dc[c][by][bx] =
@@ -1038,11 +957,7 @@ impl VarDctEncoder {
                         }
                         RAW_STRATEGY_AFV0 | RAW_STRATEGY_AFV1 | RAW_STRATEGY_AFV2
                         | RAW_STRATEGY_AFV3 => {
-                            // AFV covers 1×1 blocks, returns single DC
-                            let coeffs_arr: [f32; 64] = dct_coeffs[c][..64]
-                                .try_into()
-                                .expect("64 coefficients for AFV");
-                            let dc = dc_from_afv(&coeffs_arr);
+                            let dc = dc_from_afv(as_array_ref::<64>(&dct_coeffs[c], 0));
                             float_dc[c][by * xsize_blocks + bx] = dc;
                             let y_dc = quant_dc[1][by][bx] as f32;
                             quant_dc[c][by][bx] =
